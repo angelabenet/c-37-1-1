bf710d1ca02b37b8db1af49d1fcac15f1f42466d
ab <97624857+angelabenet@users.noreply.github.com>
2022-12-22 17:52:51 -0300

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -31,19 +31,7 @@ class Game {
     form = new Form();
     form.display();
 
-    car1 = createSprite(width / 2 - 50, height - 100);
-    car1.addImage("car1", car1_img);
-    car1.scale = 0.07;
-
-    car1.addImage("blast", blastImage);
-
-    car2 = createSprite(width / 2 + 100, height - 100);
-    car2.addImage("car2", car2_img);
-    car2.scale = 0.07;
-
-    car2.addImage("blast", blastImage);
-
-    cars = [car1, car2];
+    // adicione as sprites
 
     fuels = new Group();
     powerCoins = new Group();
